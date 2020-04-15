1a7a31e4e446830fa6e642c62773e89c83050481
Éderson <desenvolvimento@alsite.com.br>
2020-04-15 02:43:54 -0300

    DB Online MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/kontakto', { 
+mongoose.connect('mongodb+srv://ederson:dQqsP38AplJbO6z2@cluster0-bypua.mongodb.net/test?retryWrites=true&w=majority', { 
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
